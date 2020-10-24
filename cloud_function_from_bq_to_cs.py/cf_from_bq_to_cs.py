7f7a84e6253254ddf3bd7f882cfc8f6b005a5414
Vargas <a.vargas.pina@gmail.com>
2020-10-24 19:29:48 +0200

    cloud function from bq to cs several tables automated

--- cloud_function_from_bq_to_cs.py/cf_from_bq_to_cs.py
+++ cloud_function_from_bq_to_cs.py/cf_from_bq_to_cs.py
@@ -1,31 +1,36 @@
 # original source => https://stackoverflow.com/questions/59687796/how-to-schedule-an-export-from-a-bigquery-table-to-cloud-storage
 
 # Imports the BigQuery client library
+
 import os
 from google.cloud import bigquery
 
-def from_bq_to_cs(request):
-    # In ADVANCED/ENVIRONMENTAL VARIABLES (previous step), write without quotation marks (" ")
-    # key=>YOUR_PROJECT_ID   value=>the_real_name
-    project_name = os.getenv("YOUR_PROJECT_ID") 
-    bucket_name = os.getenv("YOUR_BUCKET") 
-    dataset_name = os.getenv("YOUR_DATASET") 
-    table_name = os.getenv("YOUR_TABLE") 
-    destination_uri = "gs://{}/{}".format(bucket_name, "ml_regression_data.csv.gz")
 
-    bq_client = bigquery.Client(project=project_name)
+def bigquery_request(self, request):
+     # I prefer importing os and using env variables but the configuration of the CF is neglecting them, I don't know why, need to check it out
+     project_name = "<project name>" 
+     bucket_name = "<storage bucket name withoug gs://>" 
+     dataset_name = "dataset in bigquery" 
+     table_names = ["mytable_spanish_news_economical","mytable__spanish_news_political","mytable__spanish_news_social"] 
+     
+     for t in table_names:
+          destination_uri = "gs://{}/{}.csv.gz".format(bucket_name, t)
 
-    dataset = bq_client.dataset(dataset_name, project=project_name)
-    table_to_export = dataset.table(table_name)
+     
+          bq_client = bigquery.Client(project=project_name)
 
-    job_config = bigquery.job.ExtractJobConfig()
-    job_config.compression = bigquery.Compression.GZIP
+          dataset = bq_client.dataset(dataset_name, project=project_name)
+          table_to_export = dataset.table(t)
 
-    extract_job = bq_client.extract_table(
-        table_to_export,
-        destination_uri,
-        # Location must match that of the source table.
-        location="US",
-        job_config=job_config,
-    )  
-    return "Job with ID {} started exporting data from {}.{} to {}".format(extract_job.job_id, dataset_name, table_name, destination_uri)
+          job_config = bigquery.job.ExtractJobConfig()
+          job_config.compression = bigquery.Compression.GZIP
+
+          extract_job = bq_client.extract_table(
+               table_to_export,
+               destination_uri,
+               # Location must match that of the source table.
+               location="US",
+               job_config=job_config,
+          )  
+     #return "Job with ID {} started exporting data from {}.{} to {}".format(extract_job.job_id, dataset_name, table_name, destination_uri)
+     return "Job with ID {} started exporting data from {} to {}".format(extract_job.job_id, dataset_name,bucket_name)
