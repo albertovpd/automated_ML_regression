7f043e47e5bc18b4fece9e6ce61704322ccd1482
Vargas <a.vargas.pina@gmail.com>
2020-11-06 12:50:55 +0100

    structure done

--- cloud_function_ml_regression/send_to_bucket.py
+++ cloud_function_ml_regression/send_to_bucket.py
@@ -1,3 +1,5 @@
+# this script is really redundant. I should remove it
+
 import pandas as pd
 from google.cloud import storage
 import os
@@ -6,12 +8,16 @@ load_dotenv()
 
 results_list=[
     "results-features_evolution-append.csv",
-"results-inferences-overwrite.csv",
- "results-ranking_of_features-overwrite.csv",
- "results-weekly_rmse-append.csv"
+    "results-inferences-overwrite.csv",
+    "results-ranking_of_features-overwrite.csv",
+    "results-weekly_rmse-append.csv"
  ]
 
 for r in results_list:
     df=pd.read_csv("../tmp/{}".format(r))
+
+    if 'Unnamed: 0' in df.columns:
+        df.drop(columns='Unnamed: 0', inplace=True)
+    
     df.to_csv('gs://{}/{}.csv'.format(os.getenv("bucket_in_gcs") ,r))
     print(r," sent to bucket")
