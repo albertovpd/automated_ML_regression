dbc97e64cb84fa85c043c4717381753341925047
Vargas <a.vargas.pina@gmail.com>
2020-11-06 12:39:39 +0100

    adding remove files

--- cloud_function_ml_regression/main.py
+++ cloud_function_ml_regression/main.py
@@ -1,14 +1,20 @@
 import datetime
 import numpy as np
-# Not defining datetime here will end in an error
 
 def main (data,context):
+
+    '''
+    The problem I want to solve have been separated into many steps, more than necessary
+    for good development practices, but it's easier for me to debug in this way. Myself 
+    from the future will take care of refactorizing, once the deployment works propertly. 
+    '''
     
     processes= [
         "creating_dataset.py",
         "processing_dataset.py",
-        "ml_regression.py",
-        "remove_files.py"
+        "ml_regression.py"#,
+        #"sending_to_bucket.py",
+        #"remove_files.py"
         ] 
 
     for p in processes:
