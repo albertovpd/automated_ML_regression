1b649e64632f27dfc32dc1d1adaa617c099144b4
Vargas <a.vargas.pina@gmail.com>
2020-11-06 12:00:59 +0100

    introducing env variables

--- cloud_function_ml_regression/creating_dataset.py
+++ cloud_function_ml_regression/creating_dataset.py
@@ -3,18 +3,21 @@ import pandas as pd
 from my_functions import creating_dataset
 from datetime import datetime, date
 
+from dotenv import load_dotenv
+load_dotenv()
 
-df_google = pd.read_csv("../tmp/data_pytrends.csv")
+# in .env, save the path without "", like: df_google = ./folder/whatever
+df_google= pd.read_csv(os.getenv("df_google"))
 print("google dataset loaded")
 
-df_economical=pd.read_csv("../tmp/dashboard_spanish_news_economical.csv.gz")
-df_political=pd.read_csv("../tmp/dashboard_spanish_news_political.csv.gz")
-df_social=pd.read_csv("../tmp/dashboard_spanish_news_social.csv.gz")
+df_economical=pd.read_csv(os.getenv("df_economical"))
+df_political=pd.read_csv(os.getenv("df_political"))
+df_social=pd.read_csv(os.getenv("df_social"))
 print("gdelt datasets loaded")
 
 df_google.drop(columns='Unnamed: 0', inplace=True)
 
-# creating df
+# creating df for google
 df_google_dates=pd.DataFrame()
 
 # creating the Date column in new dataset
@@ -29,15 +32,15 @@ for k in keyword_list:
     df_google_dates[k]=df_google[(df_google['keyword'] == k)]["trend_index"].tolist()
 
 
-# Using my function to create columns from keywords
+# Using my function to create columns from keywords with Gdelt datasets
 dfp = creating_dataset(df_political,"political")
 dfs = creating_dataset(df_social,"social")
 dfe = creating_dataset(df_economical,"economical")
 
 # creating final dataset with everything
 date1 = '2019-01-01'
-#date2 = datetime.now().date()
-date2="2020-10-18"
+date2 = datetime.now().date()
+#date2="2020-10-18"
 mydates = pd.date_range(date1, date2, freq="W").tolist()
 df_final=pd.DataFrame()
 df_final["date"]=mydates
