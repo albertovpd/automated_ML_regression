d89163b1e17f1a1b5b87e56cd4305badeff79671
Vargas <a.vargas.pina@gmail.com>
2020-11-07 11:44:12 +0100

    polishing script. POC

--- cloud_function_ml_regression/ml_regression.py
+++ cloud_function_ml_regression/ml_regression.py
@@ -13,7 +13,8 @@ X = df.drop(columns=["date","unemployment"]) #returns a numpy array
 #----------
 #X_for_dashboard=X.tail(1)
 X_for_dashboard=X # this run just the 1st time
-X_for_dashboard.to_csv("../tmp/results-features_evolution-append.csv")
+X_for_dashboard.to_csv("../tmp/results-features_evolution-overwrite.csv")
+# X_for_dashboard.to_csv('gs://your bucket in GCS/results-features_evolution-overwrite.csv')
 #----------
 
 X = X.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
@@ -48,12 +49,14 @@ result["infered_results"]=pd.DataFrame(rfecv.predict(X))
 
 result["infered_results"]=result["infered_results"].apply(lambda x: 0 if x<0 else round(x,2))
 result.to_csv("../tmp/results-inferences-overwrite.csv")
+#result.to_csv('gs://your bucket in GCS/results-inferences-overwrite.csv') 
 
 print("csv of inferences to tmp folder done")
 
 # weekly score
 score = pd.DataFrame({"date": [max(df["date"])], 'RMSE': [round(rfecv.score(X_train, target_train),4)]})
 score.to_csv("../tmp/results-weekly_rmse-append.csv")
+# score.to_csv('gs://your bucket in GCS/results-weekly_rmse-append.csv')
 
 # Ranking of how important are the following keywords to infer in Google searches in Spain
 # the keyword "unemployment"
@@ -63,5 +66,6 @@ features["top_important"]=rfecv.ranking_
 features.sort_values(by=["top_important"], inplace=True)
 features.reset_index(drop=True, inplace=True)
 features.to_csv("../tmp/results-ranking_of_features-overwrite.csv")
+# features.to_csv('gs://your bucket in GCS/results-ranking_of_features-overwrite.csv')
 
 print("csv of scores and evolution of features to tmp folder done")
