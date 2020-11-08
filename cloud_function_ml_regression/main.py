95ba52d8bed3ad3110486b9a2824d495fc42529a
Vargas <a.vargas.pina@gmail.com>
2020-11-08 11:49:58 +0100

    erasing redundant scripts

--- cloud_function_ml_regression/main.py
+++ cloud_function_ml_regression/main.py
@@ -6,14 +6,13 @@ def main (data,context):
     '''
     The problem I want to solve have been separated into many steps, more than necessary
     for good development practices, but it's easier for me to debug in this way. Myself 
-    from the future will take care of refactorizing, once the deployment works propertly. 
+    from the future will take care of refactoring, once the deployment works propertly. 
     '''
     
     processes= [
         "creating_dataset.py",
         "processing_dataset.py",
         "ml_regression.py"#,
-        #"sending_to_bucket.py",
         #"remove_files.py"
         ] 
 
