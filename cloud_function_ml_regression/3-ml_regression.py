5ea930998a54cf65b0109e9ab45cae466a1a5edc
Vargas <a.vargas.pina@gmail.com>
2021-02-25 23:00:52 +0100

    updating with the real cf

--- cloud_function_ml_regression/3-ml_regression.py
+++ cloud_function_ml_regression/3-ml_regression.py
@@ -8,29 +8,26 @@ from scipy import stats
 
 from datetime import datetime
 
-from my_functions import variance_threshold_selector, scientific_rounding
+from my_functions import variance_threshold_selector
 
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 
 from sklearn.feature_selection import RFECV
-from sklearn.model_selection import KFold
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
-#from sklearn.tree import DecisionTreeRegressor
-#from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import KFold, cross_validate #cross_val_score
 #
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
-
-from sklearn.metrics import mean_squared_error, explained_variance_score, max_error, mean_absolute_error,median_absolute_error, r2_score
+from sklearn.metrics import make_scorer
+import sklearn.metrics as metrics 
+from math import sqrt
+import collections
 
 df=pd.read_csv("../tmp/dataset_final_processed.csv",index_col=[0])
 
+#--  removing values than vary less than 5%
 low_v = df.drop(columns=["date","unemployment" ])  
-X_raw= variance_threshold_selector(low_v, 5) # removing values than vary less than 5%
+X_raw= variance_threshold_selector(low_v, 5) 
 
 removed=[]
 for c in low_v.columns:
@@ -38,9 +35,9 @@ for c in low_v.columns:
         #print(c)
         removed.append(c)
 removed=pd.DataFrame(removed, columns =["Removed_columns"])
-removed.to_csv("../tmp/removed_features_low_variance.csv")
-#removed.to_csv("gs://--yourbucket--/removed_features_low_variance.csv")   #<========================== 3
+    removed.to_csv("../tmp/removed_features_low_variance.csv")
 
+#-- normalization/standarization
 normalizer = preprocessing.MinMaxScaler()
 X = pd.DataFrame(normalizer.fit_transform(X_raw))
 #standardizer = preprocessing.StandardScaler()
@@ -49,7 +46,6 @@ X = pd.DataFrame(normalizer.fit_transform(X_raw))
 X.columns= X_raw.columns
 X.shape
 target=df["unemployment"]
-
 # all but last 4 rows
 X_train=X.iloc[:-4]
 target_train=target.iloc[:-4]
@@ -57,51 +53,115 @@ target_train=target.iloc[:-4]
 X_test=X.iloc[-4:]
 target_test=target.iloc[-4:]
 
-# for csvs
+# for results
 today=datetime.now().date()
 
-#----------------- ML -----------------
-regression={"Linear":   LinearRegression(),
+#----------------- ML metrics -----------------
+regression={"Linear reg":   LinearRegression(),
             "Lasso":    Lasso(alpha=0.1, selection="random", max_iter=10000, random_state=42),
             "Rigde":    Ridge(alpha=0.1,max_iter=10000, solver='auto', random_state=42)
             }
 
-min_number_features =  df.shape[0]//10 # Min number of features to play with
+metrics_dict= {
+    #"r2": metrics.r2_score, # already checked
+    "mae": make_scorer(metrics.mean_absolute_error,
+            greater_is_better=False),
+    "rmse": make_scorer(lambda y,y_pred: sqrt(metrics.mean_squared_error(y,y_pred)),
+            greater_is_better=False),
+    "mape": make_scorer(lambda y,y_pred: np.mean(np.abs(y-y_pred)/y)*100,
+            greater_is_better=False),
+    "mean_squared_error": make_scorer(metrics.mean_squared_error,
+            greater_is_better=False),
+    "explained_variance": make_scorer(metrics.explained_variance_score,
+            greater_is_better=False),
+    "max_error": make_scorer(metrics.max_error,
+            greater_is_better=False),
+    "median_absolute_error": make_scorer(metrics.median_absolute_error,
+            greater_is_better=False)
+}
 
+#----------------- ML -----------------
+min_number_features =  df.shape[0]//8 # Min number of features to play with
+
+# dictionary of r2 metrics: we'll use this for choosing the model
 model_performance={}
-for m in regression:
-    rfecv = RFECV(estimator=regression[m],
+for r in regression:
+    rfecv = RFECV(estimator=regression[r],
                 step=1, 
                   min_features_to_select=min_number_features, 
                   cv=KFold(n_splits=10,
                         shuffle=True,
                         random_state=42),
-                  scoring='neg_mean_squared_error')
+                  scoring='r2')
     rfecv.fit(X_train, target_train)
     score = rfecv.score(X_train, target_train)
+    
     if score <=0.99:
-        model_performance[score]=[m,regression[m]]
-
-model_performance = collections.OrderedDict(sorted(model_performance.items(), reverse=True)) #sort by score
-# this is according how I built the model_performance_dictionary
-score=list(model_performance.keys())[0] 
+        model_performance[score]=[r,regression[r]]
+    
+# this is to choose the winner model        
+model_performance = collections.OrderedDict(sorted(model_performance.items(), reverse=True))
+score=list(model_performance.keys())[0]
 model_name=list(model_performance.values())[0][0]
 model_config=list(model_performance.values())[0][1]
 
-print("winner model: ", model_name, ",score: ",score)
-# Ranking of how important are the following keywords to infer in Google searches in Spain
-# the keyword "unemployment"
+print(score, model_name)
+
+# final dataframe
+results={"date":today,"model":model_name, "r2":round(score,3)}
+
+# Ranking of how feature importance
 ranking_features=pd.DataFrame()
 ranking_features["features"]=X_train.columns
 ranking_features["top_important"]=rfecv.ranking_
 ranking_features.sort_values(by="top_important", ascending=True, inplace=True, ignore_index=True)
-ranking_features.to_csv("../tmp/ranking_of_features.csv")
-#ranking_features.to_csv("gs:///ranking_of_features.csv")  # <============================ 8
+ranking_features.to_csv("../tmp/ranking_of_features.csv")  # <============================
+
+# now I have the chosen features, let's use cross validation with that columns:
+chosen_features=list(ranking_features[ranking_features["top_important"]<=10]["features"])
+X_train= X[chosen_features].iloc[:-4]
+X_test=X[chosen_features].iloc[-4:]
+len(X_train.columns)
+
+# results
+results["selected_columns"]=len(X_train.columns)
+
+# Validation of model with the dictionary of metrics. I'll get a measure for every validation folder.
+evaluation = cross_validate(model_config, X_train, target_train,
+                cv = KFold(n_splits=10), scoring = metrics_dict)
+
+# just in case
+for e in evaluation:
+    evaluation[e] = evaluation[e][~np.isnan(evaluation[e])]
+    evaluation[e] = evaluation[e][~np.isinf(evaluation[e])]
+
+# populating our df of scores
+results["mae"]=round(evaluation["test_mae"].mean(),3)
+results["mae_error"]=round(evaluation["test_mae"].std(),3)
+
+results["rmse"]=round(evaluation["test_rmse"].mean(),3)
+results["rmse_error"]=round(evaluation["test_rmse"].std(),3)
+
+results["mape"]=round(evaluation["test_mape"].mean(),3)
+results["mape_error"]=round(evaluation["test_mape"].std(),3)
+
+results["mse"]=round(evaluation["test_mean_squared_error"].mean(),3)
+results["mse_error"]=round(evaluation["test_mean_squared_error"].std(),3)
+
+results["explained_var"]=round(evaluation["test_explained_variance"].mean(),3)
+results["explained_var_error"]=round(evaluation["test_explained_variance"].std(),3)
+
+results["median_abs_error"]=round(evaluation["test_median_absolute_error"].mean(),3)
+results["median_abs_error_eror"]=round(evaluation["test_median_absolute_error"].std(),3)
+
+results_df=pd.DataFrame.from_dict(results,orient='index').T
+results_df.to_csv("../tmp/weekly_score.csv") #<============================================
 
 
-# comparison real searches vs inferred results
+# inferences plot
 inferred_results=list(rfecv.predict(X)) # this is just to avoid generating nans in df
 unemployment=list(df.unemployment)
+date=df.date
 result=pd.DataFrame()
 result["date"]=df["date"]
 result["real_searches"]=unemployment
@@ -109,70 +169,7 @@ result["inferred_results"]=inferred_results
 result["inferred_results"]=result["inferred_results"].apply(lambda x: 0 if x<0 else round(x,2))
 if result["inferred_results"].min()==0:
     result.drop(result.tail(1).index,inplace=True) # drop last row (better visualization)
-result.to_csv("../tmp/results-inferences-overwrite.csv")
-#result.to_csv("gs://--yourbucket--/results-inferences-overwrite.csv") # <=============================== 5
-
-#-------------------- weekly_scores with standard deviation ------------------------------------
-mean = result.real_searches.head(-4).mean()
-N = len(result.real_searches.head(-4))
-y_real=result.real_searches.head(-4) 
-y_pred=result.inferred_results.head(-4)
-mape=round(np.abs( ((y_real-y_pred)/y_real).sum() /N )*100,3)
-
-metrics={"date":[today], 
-         "selected_columns":[rfecv.n_features_],
-         "model":model_name,
-        "mape":[mape],
-         "alg_rmse":[round(score,3)]}
-
-metrics_cross_val_score=[
-                        "neg_root_mean_squared_error",
-                        "neg_mean_squared_error",
-                         "r2",
-                         "explained_variance",
-                         "neg_mean_absolute_error",
-                         "max_error",
-                         "neg_median_absolute_error"
-                        ]
-
-regression=model_config # the winner model before
-#X_train= X_train[ranking_features[ranking_features["top_important"]==1]["features"]] #selecting the features scored as 1 in importance
-#X_train = X_train[list(X_train.columns)[:rfecv.n_features_]] # taking all the selected features before
-for m in metrics_cross_val_score:
-    score=cross_val_score(regression, 
-        X_train,
-        target_train, 
-        cv=KFold(n_splits=10, shuffle=True, random_state=42), scoring=m)
-
-    score= [-score.mean()/mean,score.std()/mean]    
-
-    metrics[m]=round(score[0],2)
-    dev="std_"+m
-    metrics[dev]=round(score[1],2)
-    if m=="neg_root_mean_squared_error":
-        metrics["rmse"]=round(score[0],2)
-        dev="std_rmse"
-        metrics[dev]=round(score[1],2)
-        metrics["std_up"]=[round(score[0]+score[1],3)]
-        metrics["std_down"]=[round(score[0]-score[1],3)]
-
-weekly_score=pd.DataFrame(metrics)
-weekly_score.rename(columns={
-                   'neg_root_mean_squared_error': "rmse", 
-                   'std_neg_root_mean_squared_error': "error_rmse",
-                   'neg_mean_squared_error':"mse", 
-                   'std_neg_mean_squared_error':"error_mse", 
-                   'std_r2':"error_r2",
-                   'std_explained_variance':"error_explained_variance",
-                    'neg_mean_absolute_error':"mae", 
-                   'std_neg_mean_absolute_error':"error_mae", 
-                   'std_max_error':"error_max_error", 
-                   'neg_median_absolute_error':"median_ae",
-                   'std_neg_median_absolute_error':"error_median_ae"}, inplace=True)
-
-weekly_score.to_csv("../tmp/weekly_score.csv") #<=====================
-#weekly_score.to_csv("gs://--yourbucket--/weekly_score.csv") #<========================= 4
-
+result.to_csv("../tmp/results-inferences-overwrite.csv") # <=============================== 5
 
 # features performance plot
 plt.figure(figsize=(16, 9))
@@ -182,5 +179,5 @@ plt.ylabel('Inner algorithm score', fontsize=28, labelpad=20)
 plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_, color='#303F9F', linewidth=3)
 plt.savefig("../tmp/RFE_columns.png")   # <================================== 7
 
-
+#
 print("ML regression done")
